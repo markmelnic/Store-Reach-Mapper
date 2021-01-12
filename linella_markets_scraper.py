19e74501702c281b703619551b3dee330107e231
Mark Melnic <markhappy200@gmail.com>
2021-01-12 18:49:58 +0200

    Minor adjustments

--- linella_markets_scraper.py
+++ linella_markets_scraper.py
@@ -3,19 +3,19 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+HEADERS = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
 # linella markets link
 link = "https://www.linella.md/markets"
 
-page = requests.get(link, headers = headers, timeout=30)
+page = requests.get(link, headers = HEADERS)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 markets = soup.find_all(class_ = 'mlm')
 
 with open("linella_markets.csv", "w", newline = '') as lm_file:
     csv_writer = csv.writer(lm_file)
-    
+
     # write first row
     csv_writer.writerow(["index", "title", "address", "timetable", "latitude", "longitude"])
 
@@ -27,5 +27,5 @@ with open("linella_markets.csv", "w", newline = '') as lm_file:
         title = market.find(class_ = "mlm-title").text
         address = market.find(class_ = "mlm-address").text
         timetable = market.find(class_ = "mlm-timetable").text
-        
+
         csv_writer.writerow([index, str.encode(title), str.encode(address), timetable, lat, lng])
